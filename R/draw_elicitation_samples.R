99b0c9947da333a6579a88673c2b9194a56a9544
javirudolphair <f.javierarudolph@gmail.com>
2023-04-01 11:08:09 -0400

    improving some documentation

--- R/draw_elicitation_samples.R
+++ R/draw_elicitation_samples.R
@@ -1,18 +1,25 @@
 #' Random draw of parameters from expert elicitation
 #'
-#' @param elicitation_data defaults to the package data on expert elicitation
-#' @param nsamples a number of samples to draw
-#' @param seed if specified, a seed is set
-#' @param return_df is TRUE the object returned is a data frame
+#' @param elicitation_data defaults to the package data on expert elicitation.
+#'   It requires a dataframe with columns for parameter name, mu, sd, and
+#'   distribution family. This function is written around the `elicitation_data`
+#'   dataset provided in this package.
+#' @param nsamples a number of samples to draw. If none set, it defaults to 1.
+#' @param seed if specified, a seed is set. If left NULL no seed is set.
+#' @param return_df if TRUE the object returned is a data frame. If FALSE it
+#'   will return a list column
 #'
 #' @return returns a tibble with list column for the samples drawn unless
 #'   otherwise specified in the `return_df` argument.
 #' @export
 #'
 #' @examples
+#' draw_elicitation_samples() # this will return the default elicitation_data with an additional column for a random sample of 1 for each parameter.
+#' draw_elicitation_samples(return_df = TRUE) # will return the same as above but expanded as a dataframe instead of a tibble with a list column
 draw_elicitation_samples <- function(elicitation_data = NULL, nsamples = NULL, seed = NULL, return_df = FALSE){
 
    if(!is.null(seed)) set.seed(seed)
+   if(is.null(nsamples)) nsamples = 1
 
    # we use the default elicitation data unless otherwise specified
    if(is.null(elicitation_data)) elicitation_data <- whitetailedSIRS::elicitation_data
