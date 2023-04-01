1f091f7ee063a77e8ce6f3f0625856f86ae47096
javirudolphair <f.javierarudolph@gmail.com>
2023-04-01 11:40:28 -0400

    minor changes to improve documentation

--- R/calc_nu_dc.R
+++ R/calc_nu_dc.R
@@ -1,11 +1,27 @@
-#' Calculate transmission for direct contact
+#' Calculate \eqn{\nu^{DC}}
 #'
-#' @param V_DC Transferred volume (ml)
-#' @param C_nu Concentration of infectious particles in sputum (gc per ml)
-#' @param pfu_conv (PFU per GC)
-#' @param k set to 410
+#' Calculate infection probability given physical contact
+#' @param V_DC Transferred volume (ml). Default is 0.1
+#' @param C_nu viral load in sputum; RNA copies/ml. Default samples from expert
+#'   elicited distribution of parameter 'Viral Load'. Concentration of infectious particles in sputum (gc per ml)
+#' @param pfu_conv conversion value (PFU per GC). Set to 1/10^5.2
+#' @param k set to 410 as the default
 #' @param nsamples number of samples wanted. Default is set to 1
-#' @param seed sets a seed for random draws of parameter from default distributions.
+#' @param seed sets a seed for random draws of parameter from default
+#'   distributions.
+#'
+#' @details Mathematical background to calculate this infection probability
+#' given physical contact. We use a Wells-Riley dose response model that
+#' estimates infection probability as a log-logistic function of dose
+#' (\eqn{d^{dc}}; plaque-forming units, PFU) and the dose-response function
+#' (\eqn{k}; Watanabe et al. 2010). The dose received is the product of a
+#' typical volume of sputum transferred given contact (\eqn{V^{DC}}) and the
+#' concentration of SARS-CoV-2 in sputum (\eqn{C_{\nu}}). We initially assume
+#' that at each contact \eqn{100\mu l} of sputum is transferred between
+#' individuals making contact.
+#' \deqn{\nu^{DC} = 1 - e^{-(d^{DC}/k)}} where
+#' \deqn{d^{DC} = V^{DC} \cdot C_{\nu} \cdot pfuConv}
+#'
 #'
 #' @return returns a number or vector of probability values for probability of infection via direct contact.
 #' @export
