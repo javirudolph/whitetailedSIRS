b82a96dd7d0195b1d59d245e5192921e71f85353
Elias Rosenblatt <erosenbl@uvm.edu>
2023-04-26 10:07:48 -0400

    Added alternative function to set up paramter vectors for SIRS ODE solve

---
+++ R/alternatives.R
@@ -0,0 +1,58 @@
+#' alternative: a function to define parameters for solving white-tailed deer SIRS ODE equations
+#'
+#'
+#' @param alpha_immunity Inverse of duration of temporary immunity after entering recovered compartment (per day, 0-1)
+#' @param c_ww Proximity rate (<=1.5m) between wild deer (proximity events per day)
+#' @param c_cw Proximity rate (<=1.5m) between captive and wild deer along fencelines(proximity events per day)
+#' @param c_cc Proximity rate (<=1.5m) between captive deer (proximity events per day)
+#' @param c_hw Proximity rate (<=1.5m) between humans and wild deer (proximity events per day)
+#' @param c_hc Proximity rate (<=1.5m) between humans and captive deer (proximity events per day)
+#' @param nu_aero_deer_deer_wild Probability of infection via aerosol transmission in wild deer
+#' @param nu_aero_deer_deer_captive Probability of infection via aerosol transmission in captive deer
+#' @param nu_aero_deer_human_wild Probability of infection via aerosol transmission from humans to wild deer
+#' @param nu_aero_deer_human_capt Probability of infection via aerosol transmission from humans to captive deer
+#' @param sigma_dc Probability of direct contact between deer, given proximity
+#' @param nu_dc_deer_deer Probability of infection via fluid transmission between deer
+#' @param gamma_recov Duration of recovery from infection (days)
+#' @param I_human Prevalence in human population (Proportion)
+#' @param boost Boosting rate in captive deer (Proportion)
+#'
+#' @details alternative() creates a list of parameter values that will be fed into a SIRS ODE solver. The length of each item in the output is determined by the length of all arguments, which must be equal. If argument(s) is not filled with outputs from another function in this package (e.g. calc_contact_rate, calc_nu_aero, calc_nu_dc, draw_elicitation_samples), the user must fill argument with vector of values of the same length as the other arguments.
+#'
+#' @return Returns a list of vectors for each parameter
+#' @export
+#'
+#' @examples
+#' alternative(alpha_immunity = c((1/30),(1/60),(1/90),(1/120),(1/150)), c_ww = rep(10,5), c_cw = rep(1,5), c_cc = rep(20,5), c_hw = rep(0.01,5), c_hc = rep(0.25,5), nu_aero_deer_deer_wild = rep(0.01,5), nu_aero_deer_deer_captive = rep(0.1,5), nu_aero_deer_human_wild = rep(0.001,5), nu_aero_deer_human_capt = rep(0.05,5), sigma_dc = rep(0.2,5), nu_dc_deer_deer = rep(0.02,5), gamma_recov = rep(1/6,5), I_human = rep(0.05,5), boost = rep(0,5))
+alternative <- function(alpha_immunity = NULL, c_ww = NULL, c_cw = NULL, c_cc = NULL, c_hw = NULL, c_hc = NULL, nu_aero_deer_deer_wild = NULL, nu_aero_deer_deer_captive = NULL, nu_aero_deer_human_wild = NULL, nu_aero_deer_human_capt = NULL, sigma_dc = NULL, nu_dc_deer_deer = NULL, gamma_recov = NULL, I_human = NULL, boost = NULL)
+
+{
+   if(is.null(alpha_immunity)) {stop("Values for alpha_immunity (duration of temporary immunity to SARS-CoV-2) is required") }
+   if(is.null(c_ww)) {stop("Values for c_ww (proximity rate between wild deer) is required") }
+   if(is.null(c_cw)) {stop("Values for c_cw (fenceline proximity rate between captive and wild deer) is required") }
+   if(is.null(c_cc)) {stop("Values for c_cc (proximity rate between captive deer) is required") }
+   if(is.null(c_hw)) {stop("Values for c_hw (proximity rate between humans and wild deer) is required") }
+   if(is.null(c_hc)) {stop("Values for c_hc (proximity rate between humans and captive deer) is required") }
+   if(is.null(nu_aero_deer_deer_wild)) {stop("Values for derived nu_aero_deer_deer_wild (probability of infection via aerosol transmission in wild deer) is required") }
+   if(is.null(nu_aero_deer_deer_captive)) {stop("Values for derived nu_aero_deer_deer_captive (probability of infection via aerosol transmission in captive deer) is required") }
+   if(is.null(nu_aero_deer_human_wild)) {stop("Values for derived nu_aero_deer_human_wild (probability of infection via aerosol transmission from humans to wild deer) is required") }
+   if(is.null(nu_aero_deer_human_capt)) {stop("Values for derived nu_aero_deer_human_capt (probability of infection via aerosol transmission from humans to captive deer) is required") }
+   if(is.null(sigma_dc)) {stop("Values for sigma_dc (probability of direct contact between deer, given proximity) is required") }
+   if(is.null(nu_dc_deer_deer)) {stop("Values for nu_dc_deer_deer (probability of infection via fluid transmission between deer) is required") }
+   if(is.null(gamma_recov)) {stop("Values for gamma_recov (duration of recovery from infection) is required") }
+   if(is.null(I_human)) {stop("Values for I_human (prevalence in human population) is required") }
+   if(is.null(boost)) {stop("Values for boost (boosting rate in captive deer) is required") }
+
+   list(
+      alpha_immunity = alpha_immunity,
+      beta_aero_ww = c_ww * nu_aero_deer_deer_wild,
+      beta_aero_cw = c_cw * nu_aero_deer_deer_wild, #assumes outdoor fenceline environment
+      beta_aero_cc = c_cc * nu_aero_deer_deer_captive,
+      beta_aero_hw = c_hw * nu_aero_deer_human_wild,
+      beta_aero_hc = c_hc * nu_aero_deer_human_capt,
+      beta_dc_ww = c_ww * sigma_dc * nu_dc_deer_deer,
+      beta_dc_cw = c_cw * sigma_dc * nu_dc_deer_deer,
+      beta_dc_cc = c_cc * sigma_dc * nu_dc_deer_deer,
+      gamma_recov = gamma_recov,
+      I_human = I_human,
+      boost = boost)}
