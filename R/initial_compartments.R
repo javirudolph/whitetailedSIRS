c9b2bad19d3aab35ce626cf325ef7be489c60bc8
Elias Rosenblatt <erosenbl@uvm.edu>
2023-05-02 12:44:14 -0400

    Added additional compartment to track cumulative infections

--- R/initial_compartments.R
+++ R/initial_compartments.R
@@ -3,9 +3,11 @@
 #' @param S_wild_prop Size of susceptible compartment in wild population (proportion)
 #' @param I_wild_prop Size of infectious compartment in wild population (proportion)
 #' @param R_wild_prop Size of recovered compartment in wild population (proportion)
+#' @param I_wild_cumulative Cumulative infected in wild segment, starting always at 0
 #' @param S_captive_prop Size of susceptible compartment in captive population (proportion)
 #' @param I_captive_prop Size of infectious compartment in captive population (proportion)
 #' @param R_captive_prop Size of recovered compartment in captive population (proportion)
+#' @param I_captive_cumulative Cumulative infected in captive segment, starting always at 0
 #' @param draws $Number of iterations of simulation
 #'
 #' @details Provides a vector of compartment sizes for wild and captive populations. This function pairs wild and captive deer populations so that transmission between populations can occur. Values (except for draws parameter) must be proportions, and must sum to one for each population.
@@ -23,6 +25,8 @@ initial_compartments <- function(S_wild_prop = 1, I_wild_prop = 0, R_wild_prop =
    S_wild = rep(S_wild_prop, draws),
    I_wild = rep(I_wild_prop, draws),
    R_wild = rep(R_wild_prop, draws),
+   I_wild_cumulative = rep(0, draws),
    S_captive = rep(S_captive_prop, draws),
    I_captive = rep(I_captive_prop, draws),
-   R_captive = rep(R_captive_prop, draws))}
+   R_captive = rep(R_captive_prop, draws),
+   I_captive_cumulative = rep(0, draws))}
